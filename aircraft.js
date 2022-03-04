5f2da71c7efd38e4a9850b42ec1225e643bafadf
Tanya-Just <98088364+Tanya-Just@users.noreply.github.com>
2022-03-04 17:11:52 +0100

    First commit

---
+++ aircraft.js
@@ -0,0 +1,6 @@
+module.exports = class Aircraft{
+    constructor(aircraftModel, seatsQuantity){
+        this.aircraftModel = aircraftModel;
+        this.seatsQuantity = seatsQuantity;
+    }
+}
---
+++ passenger.js
@@ -0,0 +1,11 @@
+module.exports = class Passenger {
+    constructor(name,age, interests,friends,children){
+        this.name = name;
+        this.age = age;
+        this.interests = [];
+        this.friends = [];
+        this.children = [];
+        this.catering = [];
+    }
+}
+
